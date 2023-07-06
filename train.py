0d55ba9e29c02b216668c114dd34121f41c36b74
d3ac <1048563083@qq.com>
2023-07-06 21:01:53 +0800

    first commit

---
+++ train.py
@@ -0,0 +1,13 @@
+import gymnasium as gym
+import torch
+import os
+import yaml
+import tqdm
+
+def main(args):
+    pass
+
+if __name__ == '__main__':
+    import argparse
+    import multiprocessing as mp
+    parser = argparse.ArgumentParser(des)

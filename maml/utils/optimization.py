f074865214e915c3b8b116f787f26f6579bf001b
d3ac <1048563083@qq.com>
2023-07-20 10:28:56 +0800

    update detach

--- maml/utils/optimization.py
+++ maml/utils/optimization.py
@@ -2,8 +2,8 @@ import torch
 
 def conjugate_gradient(f_Ax, b, cg_iters=10, residual_tol=1e-10):
     # 共轭梯度, 用于求解Ax=b的解x, 输入的是一个函数f_Ax, 
-    p = b.clone()
-    r = b.clone()
+    p = b.clone().detach()
+    r = b.clone().detach()
     x = torch.zeros_like(b).float() # 初始解x通常取0向量
     rdotr = torch.dot(r, r)
     for i in range(cg_iters):
